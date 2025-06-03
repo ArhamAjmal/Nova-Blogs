a823661d30ab7995bd00163345bca94d4a6465ce
ArhamAjmal <arhamoajmal@gmail.com>
2025-06-03 17:51:13 +0500

    Update connect.js

--- app/lib/connect.js
+++ app/lib/connect.js
@@ -1,7 +1,7 @@
 // lib/dbConnect.js
 import mongoose from 'mongoose';
 
-const MONGODB_URI = "mongodb://localhost:27017/BlogsDB";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
