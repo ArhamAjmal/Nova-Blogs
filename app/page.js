0e52ef0cd8d0a93ac760f038972e151db8b2b24b
ArhamAjmal <arhamoajmal@gmail.com>
2025-06-03 18:32:09 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,26 +8,28 @@ import Test from "./components/home/Test";
 import Footer from "./components/Footer/Footer";
 import OurTeam from "./components/home/OurTeam";
 import Contact from "./components/home/Contact";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const r=useRouter()
+  const [small,setsmall] = useState(false)
     
  useEffect(() => {
      if (typeof window !== 'undefined') {
 
  if (window.innerWidth < 720) {
   console.log("eleeeeeeeeeeee")
+  setsmall(true)
   r.push('/home')
-   return(<></>)
+  
  }
 }
    
  }, [])
-//  if (window.innerWidth < 720) {
-//   return(<></>)
-// }
+ if (small) {
+  return(<></>)
+}
 
   return (
     <div>  
