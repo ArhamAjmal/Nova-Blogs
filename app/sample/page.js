e0bc0320213e25949fa6f5b0ccde2d4d3617a376
ArhamAjmal <arhamoajmal@gmail.com>
2025-06-26 18:53:45 +0500

    Update page.js

--- app/sample/page.js
+++ app/sample/page.js
@@ -1,22 +1,11 @@
-"use client"
 import React, { useState } from 'react'
-import NotifyAdd from '../components/action/NotifyAdd'
 
 const page = () => {
-  const [show, setshow] = useState(false)
- const handleClick = () => {
-    setshow(true);
-    setTimeout(() => setshow(false), 3000); // Hide after 3s
-  };
+
   return (
     <div style={{height:"110vh"}}>
-      <button
-        onClick={handleClick}
-      >
-        Helloaaaa
-      </button>
+      
 
-     <NotifyAdd show={show} task={"add"}/>   
 </div>
   )
 }
